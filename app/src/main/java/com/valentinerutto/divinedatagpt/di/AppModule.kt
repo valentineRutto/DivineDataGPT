13b10be5d900cf29d94c99149dce7e5ea5ac2676
valentinerutto <valentinrutto@gmail.com>
2025-11-04 19:12:39 +0300

    feat: Add Ktor client to dependency injection
    - Import the `KtorClient`.
    - Provide the Ktor `httpClient` as a singleton in the `AppModule` for dependency injection.

--- app/src/main/java/com/valentinerutto/divinedatagpt/di/AppModule.kt
+++ app/src/main/java/com/valentinerutto/divinedatagpt/di/AppModule.kt
@@ -5,6 +5,7 @@ import com.valentinerutto.divinedatagpt.MyApplication
 import com.valentinerutto.divinedatagpt.data.DivineDataRepository
 import com.valentinerutto.divinedatagpt.data.local.DivineDatabase
 import com.valentinerutto.divinedatagpt.data.network.ApiService
+import com.valentinerutto.divinedatagpt.data.network.KtorClient
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.dsl.viewModel
 import org.koin.core.scope.Scope
@@ -14,6 +15,7 @@ val AppModule = module{
 
     single { MyApplication.INSTANCE }
 
+single{ KtorClient.httpClient }
     single { ApiService() }
     single { DivineDataRepository(get()) }
 
