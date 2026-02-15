796d27bddf07df747721aae21157c40d5a97318a
valentinerutto <valentinrutto@gmail.com>
2026-02-15 20:23:11 +0300

    chore: Increase minSdk to 26
    - Update `minSdk` version from 24 to 26 in `app/build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
     
     defaultConfig {
         applicationId = "com.valentinerutto.divinedatagpt"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
