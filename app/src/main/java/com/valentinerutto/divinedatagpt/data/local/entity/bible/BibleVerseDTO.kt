28a08ea1e4dfd52f1c53312e7fc56d22c88d8834
valentinerutto <valentinrutto@gmail.com>
2026-03-05 20:39:25 +0300

    feat(data): Add BibleVerseDto for local bible entity mapping
    - Introduce `BibleVerseDto` data class to handle the serialization of Bible verse data.
    - Include fields for book metadata (ID, name, chapter, verse) and multiple translations (KJV, WEB, Leningrad Codex, etc.) using `@SerializedName` annotations.

---
+++ app/src/main/java/com/valentinerutto/divinedatagpt/data/local/entity/bible/BibleVerseDTO.kt
@@ -0,0 +1,52 @@
+package com.valentinerutto.divinedatagpt.data.local.entity.bible
+
+
+import com.google.gson.annotations.SerializedName
+
+data class BibleVerseDto(
+
+    @SerializedName("id")
+    val id: Int,
+
+    @SerializedName("book_id")
+    val bookId: Int,
+
+    @SerializedName("book_name")
+    val bookName: String,
+
+    @SerializedName("chapter")
+    val chapter: Int,
+
+    @SerializedName("verse")
+    val verse: Int,
+
+    @SerializedName("world_english_bible_web")
+    val worldEnglishBibleWeb: String? = null,
+
+    @SerializedName("king_james_bible_kjv")
+    val kingJamesBibleKjv: String? = null,
+
+    @SerializedName("leningrad_codex")
+    val leningradCodex: String? = null,
+
+    @SerializedName("jewish_publication_society_jps")
+    val jewishPublicationSocietyJps: String? = null,
+
+    @SerializedName("codex_alexandrinus")
+    val codexAlexandrinus: String? = null,
+
+    @SerializedName("brenton")
+    val brenton: String? = null,
+
+    @SerializedName("samaritan_pentateuch")
+    val samaritanPentateuch: String? = null,
+
+    @SerializedName("samaritan_pentateuch_english")
+    val samaritanPentateuchEnglish: String? = null,
+
+    @SerializedName("onkelos_aramaic")
+    val onkelosAramaic: String? = null,
+
+    @SerializedName("onkelos_english")
+    val onkelosEnglish: String? = null
+)
