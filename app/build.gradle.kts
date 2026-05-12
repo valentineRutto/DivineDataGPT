691fe88e3f57fbe86a0b0fff77b1ffd74fda3ab4
valentinerutto <valentinrutto@gmail.com>
2026-05-12 18:13:21 +0300

    remove unused classes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,8 +5,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.ksp)
-    alias(libs.plugins.kotlinserialization)
+        alias(libs.plugins.ksp)
+        alias(libs.plugins.kotlinserialization)
 
 }
 
